f435af8ed52a2346e83a04ab32a5a88f2f0cd4ce
user <user@sec>
2015-08-28 16:41:26 +0900

    e

---
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -0,0 +1,21 @@
+package org.dimigo.inheritance;
+
+public class Galaxy extends SmartPhone{
+	
+	public Galaxy(String model, String company, int price){
+		super(model, company, price);
+	}
+	
+	public Galaxy(){
+		super();
+	}
+	
+	public void pay() {
+		System.out.println("삼성 페이로 결제합니다.");
+	}
+	
+	public void useSpecialFunction(){
+		System.out.println("무선충전 기능을 이용합니다.");
+	}
+
+}
---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,21 @@
+package org.dimigo.inheritance;
+
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		Galaxy g = new Galaxy("갤럭시 S6", "삼성", 650000);
+		IPhone i = new IPhone("iPhone6", "애플", 700000);
+		
+		i.turnOn();
+		i.pay();
+		i.useSpecialFunction();
+		i.turnOff();
+		
+		g.turnOn();
+		g.pay();
+		g.useSpecialFunction();
+		g.turnOff();
+
+	}
+
+}
