133a7a15cdd0248d79d4801e58732367dbbe7882
user <user@sec>
2015-10-02 16:09:27 +0900

    d

---
+++ JavaPractice/src/org/dimigo/collection/Music.java
@@ -0,0 +1,16 @@
+package org.dimigo.collection;
+
+public class Music {
+
+	
+		public String title;
+		public String singer;
+		public Music(String title, String singer){
+			this.title = title;
+			this.singer = singer;
+		}
+		
+
+	}
+
+
